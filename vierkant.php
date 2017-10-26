c293ffb0a60f8f7490b2deb5ead00809f1ae934b
Anne-Marije <napannemarije@gmail.com>
2017-10-26 14:43:18 +0200

    eerste

---
+++ vierkant.php
@@ -0,0 +1,48 @@
+
+<html>
+    <head>
+        <style>
+            body{
+                font-family: 'courier';
+            }
+        </style>
+    </head>
+
+
+
+
+
+    <?php
+    $vulCharacter = "u";
+    $regel = "";
+    $multiplier = 20;
+    $teetje = "T";
+//$bovensteRegel = "________________________________________________________";
+    $bovensteRegel = str_repeat($teetje, $multiplier + 2);
+
+
+//        echo "<table>  {border: 1px solid black;}<tr><th>oooooooooooooooooooooooooooooooooooooooo</th></tr></table>"
+//
+//
+//        "-----------------------------------------------";
+    echo $bovensteRegel;
+     $plaats = 0;
+    for ($i = 0; $i <= $multiplier; $i++) {    // loops vertikaal
+//    echo $i . "<br>";
+        echo "<br>" . $teetje;
+       
+       $plaats++;
+        for ($j = 0; $j <= $multiplier; $j++) {  // loop om charactersw  horisontaal neeer te zetten
+            if ($j == $plaats) {
+                echo "&nbsp";
+            } else {
+                echo"o";  // dit is het fvul chracter 
+            }
+        }
+        echo $teetje;
+    }
+    $ondersteRegel = "<br>" . str_repeat($teetje, $multiplier + 2);
+    echo $ondersteRegel
+    ?>
+
+</html>
