8015801be79e9ca9f34e58cfe863d553c3bdfcd4
Anne-Marije <napannemarije@gmail.com>
2017-10-26 15:05:23 +0200

    4

--- vierkant.php
+++ vierkant.php
@@ -8,51 +8,54 @@
         </style>
     </head>
     <?php
-    
-    tekenVierkant("U");
-    tekenVierkant("q");
-    tekenVierkant("E");
-    tekenVierkant("F");
-    tekenVierkant("G");
-    tekenVierkant("H");
-    
-    
-    function tekenVierkant($vulCharacter){
+    tekenVierkant("U","windows");
+    tekenVierkant("q","unis");
+    tekenVierkant("E","windows");
+    tekenVierkant("F","unis");
+    tekenVierkant("G","windows");
+    tekenVierkant("H","unis");
+
+    function tekenVierkant($vulCharacter, $richting) {
 //    $vulCharacter = "u";
-    $regel = "";
-    $multiplier = 20;
-    $teetje = "T";
+        $regel = "";
+        $multiplier = 20;
+        $teetje = "T";
 //$bovensteRegel = "________________________________________________________";
-    $bovensteRegel = "<br>".str_repeat($teetje, $multiplier + 2);
+        $bovensteRegel = "<br>" . str_repeat($teetje, $multiplier + 2);
 
 
 //        echo "<table>  {border: 1px solid black;}<tr><th>oooooooooooooooooooooooooooooooooooooooo</th></tr></table>"
 //
 //
 //        "-----------------------------------------------";
-    echo $bovensteRegel;
-     $plaats = $multiplier;
-    for ($i = 0; $i <= $multiplier; $i++) {    // loops vertikaal
-//    echo $i . "<br>";
-        echo "<br>" . $teetje;
-       
-       $plaats--;
-        for ($j = 0; $j <= $multiplier; $j++) {  // loop om charactersw  horisontaal neeer te zetten
-            if ($j == $plaats) {
-                echo "&nbsp";
-            } else {
-                echo $vulCharacter;  // dit is het fvul chracter 
-            }
+        echo $bovensteRegel;
+        if ($richting == "windows") {
+            $plaats = $multiplier;
+        } else {
+            $plaats = 0;
         }
-        echo $teetje;
+
+        for ($i = 0; $i <= $multiplier; $i++) {    // loops vertikaal
+//    echo $i . "<br>";
+            echo "<br>" . $teetje;
+            if ($richting == "windows") {
+                $plaats--;
+            } else {
+                $plaats++;
+            }
+
+            for ($j = 0; $j <= $multiplier; $j++) {  // loop om charactersw  horisontaal neeer te zetten
+                if ($j == $plaats) {
+                    echo "&nbsp";
+                } else {
+                    echo $vulCharacter;  // dit is het fvul chracter 
+                }
+            }
+            echo $teetje;
+        }
+        $ondersteRegel = "<br>" . str_repeat($teetje, $multiplier + 2);
+        echo $ondersteRegel;
     }
-    $ondersteRegel = "<br>" . str_repeat($teetje, $multiplier + 2);
-    echo $ondersteRegel;
-    }
-    
-    
-            
-            
     ?>
 
 </html>
